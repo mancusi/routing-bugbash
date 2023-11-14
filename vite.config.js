37fba9c4794bff4e640febb5cf63b1a387b39139
Justin Mancusi <jmancusi@yext.com>
2023-11-14 15:40:18 -0500

    update prefix

--- vite.config.js
+++ vite.config.js
@@ -14,6 +14,6 @@ export default defineConfig({
   plugins: [react(), yextSSG(), vitePlugin()],
   publicDir: "/public",
   build: {
-    assetsDir: "simulated-prefix/assets"
+    assetsDir: "prefix/assets"
   }
 });
